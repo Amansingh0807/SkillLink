6b769f112227e0c26037e6274d63a4e736a0981b
Aman Singh <amansingh080704@gmail.com>
2025-06-24 21:33:02 +0530

    Initial Changes

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-4xl font-bold">Welcome to Our Website</h1>
+      <p className="mt-4">We are glad to have you here.</p>
+    </div>
+  )
+}
